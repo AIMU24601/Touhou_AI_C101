97eee882d700dbf684e8f4c4784375a834b3e171
Nemo <38112258+AIMU24601@users.noreply.github.com>
2022-08-18 12:19:37 +0900

    背景有の画像を生成できるように

--- ThGen07/ThGen07.py
+++ ThGen07/ThGen07.py
@@ -4,15 +4,25 @@ from PIL import Image
 import random
 import numpy as np
 import math
+import os
 
 number_player = 1
 min_number_enemy = 1
 max_number_enemy = 10
 min_number_bullet = 1
 max_munber_bullet = 100
+MIN_NUMBER_BG_PARTS = 1
+MAX_NUMBER_BG_PATRS = 10
 player_list = list()
 enemy_list = list()
 bullet_list = list()
+bg_list = list()
+bg_pars_list = list()
+
+DATA_DIR = "E:/touhou/data/7/"
+
+bg_dir = os.path.join(DATA_DIR, "bg_tmp/")
+bg_parts_dir = os.path.join(DATA_DIR, "bg_parts_tmp/")
 
 #貼り付ける画像をロード
 for i in range(1, 23):
@@ -27,9 +37,21 @@ for i in range(1, 173):
     tmp = Image.open("E:/touhou/data/7/bullet_tmp/" + str(i) + ".png")
     bullet_list.append(tmp)
 
+ids = os.listdir(bg_dir)
+image_ids = [os.path.join(bg_dir,image_id) for image_id in ids]
+
+for i in range(len(image_ids)):
+    bg_list.append(Image.open(image_ids[i]))
+
+ids = os.listdir(bg_parts_dir)
+image_ids = [os.path.join(bg_parts_dir,image_id) for image_id in ids]
+
+for i in range(len(image_ids)):
+    bg_pars_list.append(Image.open(image_ids[i]))
+
 #ゲームのスクリーンショットにサイズを合わせる→160*120にするための比を求める
 img = cv2.imread("../screenshot.jpg")
-target_x = 120
+target_x = 128
 target_y = 160
 row = len(img)
 row_ratio = target_y/row
@@ -39,9 +61,26 @@ column_raio = target_x/column
 def ThGen(t):
     #画像を貼り付ける所を作成
     train = np.zeros((target_y, target_x))
+    bg = np.zeros((target_y, target_x))
     annot = np.zeros((target_y, target_x)) # 0 means this pixel does not belong to any classes
     #ndarrayはcv2で使うものなのでPILに変換する
-    train = Image.fromarray(train, "RGBA")
+    train = Image.fromarray(train,"RGBA")
+    bg = Image.fromarray(bg,"RGBA")
+
+    #背景用の画像を生成
+    b = 1
+    for i in range(1):
+        tmp = random.randint(0,len(bg_list)-1) #貼り付ける背景を決定
+        im = bg_list[tmp]
+        bg.paste(im)
+
+    b_parts = random.randint(MIN_NUMBER_BG_PARTS,MAX_NUMBER_BG_PATRS)
+    for i in range(b):
+        x = random.randint(0, target_x-1)
+        y = random.randint(0,target_y-1)
+        tmp = random.randint(0,len(bg_pars_list)-1)
+        im = bg_pars_list[tmp]
+        bg.paste(im,(x,y),mask=im)
 
     e = random.randint(min_number_enemy, max_number_enemy)
     for i in range(e):
@@ -100,8 +139,8 @@ def ThGen(t):
                     if sum(tmp) >= (120+255):
                         annot[j][k] = 1 #one-hot of player
 
-    train.save("E:/touhou/resize/train/7/" + str(t) + ".png")
-    np.save("E:/touhou/resize/train_annot/7/" + str(t), annot)
+    #train.save("E:/touhou/resize/train/7/" + str(t) + ".png")
+    np.save("E:/touhou/resize/with_background/train_annot/7/" + str(t), annot)
 
     eval_save = np.zeros((target_y, target_x, 3))
     for j in range(target_y):
@@ -112,10 +151,13 @@ def ThGen(t):
                     eval_save[j][k] = [0,255, 0]
                 elif annot[j][k] == 3: # this pixel belongs to bullet class
                     eval_save[j][k] = [0, 0, 255]
-    cv2.imwrite("E:/touhou/resize/annot_img/7/" + str(t) + ".png", eval_save)
+    cv2.imwrite("E:/touhou/resize/with_background//annot_img/7/" + str(t) + ".png", eval_save)
+
+    bg.paste(train,mask=train)
+    bg.save("E:/touhou/resize/with_background/train/7/"+str(t)+".png")
 
 if __name__ == "__main__":
-    for i in range(0, 21101):
+    for i in range(0, 20):
         ThGen(i)
         if i %100 == 0:
             print(i)
